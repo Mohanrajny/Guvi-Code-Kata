b5d48a738c4faec5302e6c658744795941305d2c
Mohanrajny <mohanraj746007@gmail.com>
2017-09-17 14:47:02 +0530

    Add files via upload

---
+++ Even or Odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int no;
+    scanf("%d",&no);
+    if (no%2==0)
+        printf("It Is Even Number");
+    else
+        printf("It Is Odd Number");
+    return 0;
+}
